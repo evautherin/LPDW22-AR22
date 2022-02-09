8eac627e7c08bc8712e3cc08fe04631b57f0dcc4
Etienne Vautherin <etienne@vautherin.com>
2022-02-09 12:04:33 +0100

    Replace AppDelegate by AR22App

---
+++ AR22/AR22App.swift
@@ -0,0 +1,17 @@
+//
+//  TuesdayApp.swift
+//  Tuesday
+//
+//  Created by Etienne Vautherin on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AR22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
